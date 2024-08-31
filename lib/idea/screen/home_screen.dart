5540546dacf874df0e71395a9e33f2a588c06093
Ibrahim0096891 <ibrahim0096891@gmail.com>
2024-08-31 12:04:29 +0600

    responsible32

--- lib/idea/screen/home_screen.dart
+++ lib/idea/screen/home_screen.dart
@@ -20,165 +20,218 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           backgroundColor: Colors.grey.shade100,
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.green),
+        drawer: _buildDrawer(),
+        body: ResponsibeBuilder(
+          mobile: _buildBody(),
+          tablet: _buildBody(),
+          desktop: Center(
+            child: Text('desktop'),
+          ),
+        ),
+      ),
+      desktop: Scaffold(
+        appBar: _desktopAppBar(),
+        body: _desktopBody(),
+      ),
+    );
+  }
+
+  Widget _desktopBody() {
+    return Padding(
+        padding: const EdgeInsets.only(top: 80,left: 20),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'FLUTTER WEB.\nTHE BASICS',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'In this course we will go over the bascis of uing \nFlutter Web for development. Topics will include \nResponsible Layout, Deploying, Font Changes, Hover \nfunctionality, Modals and more',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 220),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'SKILL UP NOW',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
+                    // SizedBox(width: 500),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Join Course',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      'TAP HERE',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(200, 50),
+                        backgroundColor: Colors.green,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-              ListTile(
-                leading: Icon(
-                  Icons.movie_filter_outlined,
-                  color: Colors.black,
-                ),
-                title: Text('Episodes'),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.info,
-                  color: Colors.black,
-                ),
-                title: Text('About'),
-              ),
-            ],
-          ),
-        ),
-        body: ResponsibeBuilder(
-          mobile: Center(
-            child: Text('mobile'),
-          ),
-          tablet: Center(
-            child: Text('tablet'),
-          ),
-          desktop: Container(
-            color: Colors.white,
-            child: Center(
-              child: Text('desktop'),
-            ),
-          ),
-        ),
-      ),
-      desktop: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 100,
-          title: Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Text('HUMMING\nBIRD.'),
-          ),
-          backgroundColor: Colors.white,
-          actions: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                'Episodes',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Center(
-                child: Text(
-                  'About',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-            ),
+            )
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 80,left: 20),
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
+      );
+  }
+
+  AppBar _desktopAppBar() {
+    return AppBar(
+        toolbarHeight: 100,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Text('HUMMING\nBIRD.'),
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'Episodes',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Center(
+              child: Text(
+                'About',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+            ),
+          ),
+        ],
+      );
+  }
+
+  Widget _buildBody() {
+    return Center(
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'FLUTTER WEB.\nTHE BASIC',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'FLUTTER WEB.\nTHE BASICS',
-                            style: TextStyle(
-                              fontSize: 36,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          Text(
-                            'In this course we will go over the bascis of uing \nFlutter Web for development. Topics will include \nResponsible Layout, Deploying, Font Changes, Hover \nfunctionality, Modals and more',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'In this course we will go over the bascis of uing \nFlutter Web for development. Topics will include \nResponsible Layout, Deploying, Font Changes, Hover \nfunctionality, Modals and more',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Join Course',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(300, 50) ,
+                      backgroundColor: Colors.green,
+                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                ),
+              ],
+            ),
+          ),
+        );
+  }
+
+  Widget _buildDrawer() {
+    return Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.green),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'SKILL UP NOW',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'TAP HERE',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
                   ),
                 ],
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 220),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // SizedBox(width: 500),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Join Course',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(200, 50),
-                          backgroundColor: Colors.green,
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.movie_filter_outlined,
+                color: Colors.black,
+              ),
+              title: Text('Episodes'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.info,
+                color: Colors.black,
+              ),
+              title: Text('About'),
+            ),
+          ],
         ),
-      ),
-    );
+      );
   }
 }
